d44ba7b7716195a8d29bf0ac5be386341cd68581
Damien <dasseya1@gmail.com>
2017-07-18 18:56:16 -0400

    Set-up the main file app.js

---
+++ app/app.js
@@ -0,0 +1,8 @@
+// Include the Main React Dependencies
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+// This code here allows us to render an entire block of Bootstrap layout HTML using
+// our Main Component
+render(routes, document.getElementById("app"));
