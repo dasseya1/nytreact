e139c3b089fcee02847f01524f7df8c6145467c9
Damien <dasseya1@gmail.com>
2017-07-18 23:39:28 -0400

    API call functionality

--- server.js
+++ server.js
@@ -12,6 +12,51 @@ app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 app.use(express.static("./public"));
 
+app.get("/api/saved", function(req, res) {
+
+var query = Article.find({});
+    query.exec(function(err, articles) {
+    if (err) {
+      console.log(err);
+    }
+      res.json(articles);
+  });
+});
+
+app.post("/api", function(req, res) {
+
+    var art = new Article({
+        title: req.body.title,
+        url: req.body.url
+    });
+
+    art.save(function(err, art) {
+        // If there's an error during this query
+        if (err) {
+            // Log the error
+            return console.log(err);
+        }
+        // Otherwise,
+        else {
+            //log results
+      res.end();
+        }
+    });
+
+});
+
+
+// removes saved articles from db
+app.delete('/api/saved/:id', function(req, res){
+	Article.findOneAndRemove({"_id": req.params.id},function(err, article) {
+
+		if (err) {
+			console.log(err);
+		}
+		    res.end();
+		})
+	})
+
 
 //Main '/' Route. This will return the home page
 app.get("*", function(req, res) {
