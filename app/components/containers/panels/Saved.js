4513f4ee31986bd7164f67805692d0a28b2ad9d0
Damien <dasseya1@gmail.com>
2017-07-18 21:36:39 -0400

    Create component for single Saved item

---
+++ app/components/containers/panels/Saved.js
@@ -0,0 +1,13 @@
+// Include React
+import React from 'react';
+
+// Create the Saved Component
+
+const Saved = (props) => {
+    return (
+        <div>{props.articles}</div>
+    );
+};
+
+// Export the component back for use in other files
+export default Saved;
