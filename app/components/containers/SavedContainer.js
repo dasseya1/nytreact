cc5ca4523734e70ad796ae56f391e0e2deb3a9e2
Damien <dasseya1@gmail.com>
2017-07-18 21:24:51 -0400

    Saved and Seach Components

---
+++ app/components/containers/SavedContainer.js
@@ -0,0 +1,90 @@
+// Include React
+var React = require("react");
+var helpers = require("../../utils/nytHelpers");
+var Saved = require("./panels/Saved");
+
+const SavedContainer = React.createClass({
+
+    getInitialState: function() {
+        return {articles: []}
+    },
+
+    componentDidMount: function() {
+        // Get the saved articles
+        helpers.getArticles().then(function(response) {
+            var component = this;
+            if (response !== this.state.articles) {
+
+                var articulo = response.map(function(arts, i) {
+
+                    return <div className="well" key={i}>
+                        <h4 className="articleHeadline">
+                            <span className="label label-success">{i + 1}</span>
+                            {arts.title}</h4>
+                        <h5>{arts.date}</h5>
+                        <a href={arts.url}>{arts.url}</a>
+
+                        <input type="hidden" name="articleId" value={arts._id}/>
+                        <br></br>
+                        <button onClick={() => component.removeArticleClick(arts._id)} className="btn btn-default text-center btn-primary">Delete</button>
+                    </div>;
+                })
+
+                this.setState({articles: articulo});
+            }
+        }.bind(this));
+    },
+
+    removeArticleClick: function(response) {
+        helpers.deleteArticle(response)
+        console.log(response);
+        // update state of parent
+        helpers.getArticles().then(function(response) {
+            var component = this;
+            if (response !== this.state.articles) {
+
+                var articulo = response.map(function(arts, i) {
+
+                    return <div className="well" key={i}>
+                        <h4 className="articleHeadline">
+                            <span className="label label-success">{i + 1}</span>
+                            {arts.title}</h4>
+                        <h5>{arts.date}</h5>
+                        <a href={arts.url}>{arts.url}</a>
+
+                        <input type="hidden" name="articleId" value={arts._id}/>
+                        <br></br>
+                        <button onClick={() => component.removeArticleClick(arts._id)} className="btn btn-default text-center btn-primary">Delete</button>
+                    </div>;
+                })
+
+                this.setState({articles: articulo});
+            }
+        }.bind(this));
+
+    },
+
+    render: function() {
+        return (
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-md-12">
+                        <div className="panel panel-success">
+                            <div className="panel-heading">
+                                <h3 className="panel-title text-center">Saved Articles</h3>
+                            </div>
+                            <div className="panel-body">
+                                <Saved articles={this.state.articles} deleteArticle={this.deleteArticle}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+});
+
+module.exports = SavedContainer;
---
+++ app/components/containers/SearchContainer.js
@@ -0,0 +1,91 @@
+var React = require("react");
+var helpers = require("../../utils/nytHelpers");
+var Search = require("./panels/Search");
+
+const SearchContainer = React.createClass({
+
+    getInitialState: function() {
+
+        return {queryTerm: ""};
+    },
+    saveArticle: function(response) {
+
+        helpers.postArticle(response)
+
+        // update state of parent
+        helpers.getArticles().then(function(response) {
+            var component = this;
+            if (response !== this.state.articles) {
+
+                var articulo = response.map(function(arts, i) {
+
+                    return <div className="well" key={i}>
+                        <h4 className="articleHeadline">
+                            <span className="label label-success">{i + 1}</span>
+                            {arts.title}</h4>
+                        <h5>{arts.date}</h5>
+                        <a href={arts.url}>{arts.url}</a>
+
+                        <input type="hidden" name="articleId" value={arts._id}/>
+                        <br></br>
+                        <button onClick={() => component.removeArticleClick(arts._id)} className="btn btn-success">Delete</button>
+                    </div>;
+                })
+
+                this.setState({articles: articulo});
+                alert('article saved!');
+            }
+        }.bind(this));
+
+    },
+
+    componentDidUpdate: function() {
+
+        helpers.runQuery(this.state.queryTerm, this.state.startYear, this.state.endYear).then(function(data) {
+            var component = this;
+            var articulo = data.map(function(art, i) {
+
+                var artTitle = art.headline.main;
+                var artUrl = art.web_url;
+
+                return <div className="well" key={i}>
+                    <h4 className="articleHeadline">
+                        <span className="label label-primary">{i + 1}</span>
+                        {art.headline.main}</h4>
+                    <h5>{art.pub_date}</h5>
+                    <h5>{art.section_name}</h5>
+                    <p>{art.snippet}</p>
+                    <a href={art.web_url}>{art.web_url}</a>
+                    <form method="POST" action="/api">
+                        <input type="hidden" name="title" value={art.headline.main}/>
+                        <input type="hidden" name="url" value={art.web_url}/>
+                        <br></br>
+                        <button onClick={() => component.saveArticle(art)} className="btn btn-primary">Save</button>
+
+                    </form>
+                </div>;
+            })
+
+            this.setState({results: articulo});
+
+        }.bind(this));
+
+    },
+
+    setTerm: function(term) {
+        this.setState({queryTerm: term});
+    },
+    // Here we describe this component's render method
+    render: function() {
+        return (
+            <div>
+                <Search setTerm={this.setTerm} setStartYear={this.startYear} setEndYear={this.endYear} results={this.state.results}/>
+
+            </div>
+
+        );
+    }
+
+});
+
+module.exports = SearchContainer;
